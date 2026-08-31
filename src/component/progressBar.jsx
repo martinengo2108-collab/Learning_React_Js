24dcb51870662f2c9c13a51bbf095ad76106c821
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-31 15:29:13 +0100

    exporting progress bar function

--- src/component/progressBar.jsx
+++ src/component/progressBar.jsx
@@ -1,6 +1,14 @@
 export default function progressBar({percent, goal , raised}){
 
     const clamped = Math.min(Math.max(percent,0),100);
-    const ticks = [0,]
+    const ticks = [0,25,50,75,100]
+
+    return(
+        <div className="progress-gauge">
+            <div className="progress-gauge__header">
+                
+            </div>
+        </div>
+    )
 
 }
