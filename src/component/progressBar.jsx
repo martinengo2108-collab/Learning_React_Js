fa9d2a545a98b2140bb8335268cdd313dd6fc6b8
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-31 15:43:05 +0100

    exporting progress bar function

--- src/component/progressBar.jsx
+++ src/component/progressBar.jsx
@@ -6,7 +6,21 @@ export default function progressBar({percent, goal , raised}){
     return(
         <div className="progress-gauge">
             <div className="progress-gauge__header">
-                
+                <span className="progress-gauge__percent">
+                    {clamped.toFixed(0)}% funded
+                </span>
+                <span className="progress-gauge__goal">
+                    Goal: {goal.toLocaleString()} FCFA
+                </span>
+            </div>
+            <div 
+            className="progress-guage__track"
+            role="progressbar"
+            aria-valuenow={Math.round(clamped)}
+            aria-valuemin={0}
+            aria-valuemax={100}
+            aria-label={`${clamped.toFixed(0)} percent of fundraising goal reached`}>
+
             </div>
         </div>
     )
