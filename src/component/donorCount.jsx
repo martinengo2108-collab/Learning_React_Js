ec7b42831261a177355f76521ae422588dbe5d80
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-31 14:41:57 +0100

    adding span

--- src/component/donorCount.jsx
+++ src/component/donorCount.jsx
@@ -1,8 +1,12 @@
 export default function DonorCount({count}){
     return(
         <div className="stat-card">
-            
+            <span className="stat-label">Donors</span>
+            <span className="stat-value">{count}</span>
+            <span className="stat-sublabel">
+                {count === 1 ? "person has given" : "people have given"}
+            </span>
 
         </div>
-    )
-}
+    );
+};
