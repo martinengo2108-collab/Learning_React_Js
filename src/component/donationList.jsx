fde041d108c7ed5d7eb952ddd353bc080c398ab0
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-31 23:06:14 +0100

    donation list additions

--- src/component/donationList.jsx
+++ src/component/donationList.jsx
@@ -1,5 +1,22 @@
 import donationItem from "./donationItem.jsx"
 
 export default function donationList({donations ,onDeleteDonation}){
-    
+    return (
+        <div className="donation-list-card">
+            <h2 className="form-title">Recent donations</h2>
+
+            {donations.length ===0 ? (
+                <p className="empty-state">Add donations to get started.</p>
+            ):(
+                <ul className="donation-list">
+                    {donations.map((donation) =>(
+                        <donationItem 
+                        key ={donation.id}
+                        donation={donation}
+                        onDelete={onDeleteDonation}/>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
 }
