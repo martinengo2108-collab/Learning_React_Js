76683a3698614a2899a8eba792916cfde16f18ae
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-01 11:13:47 +0100

    improving structure in goal editor file

--- src/component/goalEditor.jsx
+++ src/component/goalEditor.jsx
@@ -3,4 +3,27 @@ import { useState } from "react";
 export default function GoalEditor({ goal,onUpdateGoal }) {
     const [isEditing, setIsEditing] = useState(false);
     const [value, setValue] = useState(String(goal));
+    const [error, setError] = useState("");
+
+    function startEditing() {
+        setValue(String(goal));
+        setError("");
+
+        setIsEditing(true);
+    }
+
+    function handleSave(event){
+        event.preventDefault();
+        const numericValue = Number(value);
+        if (value.trim() === "" || Number.isNaN(numericValue) || numericValue <=0){
+            setError("The Goal must be a relevant amount");
+            return;
+        }
+        onUpdateGoal(numericValue);
+        setIsEditing(false);
+    }
+    if(!isEditing){
+
+        
+    }
 }
