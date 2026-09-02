032bdcd3f72e623a9dc4796b78760bf26390ee3b
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-02 11:33:17 +0100

    importing files in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,10 @@ import ReactDom from "react-dom/client";
 
 import App from "App.jsx";
 import "./App.css";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+);
