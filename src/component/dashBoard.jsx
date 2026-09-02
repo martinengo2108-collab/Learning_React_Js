cc9427b1cc4e44292b0f53eabf655f64b7aa538f
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-02 12:04:01 +0100

    importing files

---
+++ src/component/dashBoard.jsx
@@ -0,0 +1,7 @@
+import TotalRaised from "./totalRaised.jsx";
+import DonorCount from "./donorCount.jsx";
+
+
+import ProgressBar from "./progressBar.jsx";
+import DonationForm from "./donationForm.jsx";
+import DonationList from "./donationList.jsx";
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-import App from "./App.jsx";
+import App from "App.jsx";
 import "./App.css";
 
 ReactDom.createRoot(document.getElementById("root")).render(
