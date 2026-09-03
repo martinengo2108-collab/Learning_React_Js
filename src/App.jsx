2dc0e0805c1e1504369bcc4a51d8447b07dae55a
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-03 22:13:40 +0100

    importing components

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React,{ useState, useEffect } from 'react';
+
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import TerminalLog from './components/TerminalLog';
