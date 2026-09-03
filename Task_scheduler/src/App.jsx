b0b86b73b467b0cbc09c7157694df790eb12ebbc
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-03 23:06:12 +0100

    formatting

--- Task_scheduler/src/App.jsx
+++ Task_scheduler/src/App.jsx
@@ -1,52 +1,52 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import TerminalLog from './components/TerminalLog';
 
-export default function App() { 
-    const [tasks, setTasks] = useState(() =>{
+export default function App() {
+    const [tasks, setTasks] = useState(() => {
         const savedTasks = localStorage.getItem('tasks');
         return savedTasks ? JSON.parse(savedTasks) : [];
     });
 
-    const[Logs, setLogs] = useState([]);
+    const [Logs, setLogs] = useState([]);
 
-    const addLog =(sender,message,type ='info') =>{
+    const addLog = (sender, message, type = 'info') => {
 
         const timeStamp = new Date().toLocaleTimeString();
-        setLogs(prev => [...prev, {sender,message,type,timeStamp}]);
+        setLogs(prev => [...prev, { sender, message, type, timeStamp }]);
     };
 
     useEffect(() => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
 
-        const activeIntervals =[];
+        const activeIntervals = [];
 
-        tasks.forEach((task) =>{
-            if(task.status ==="active"){
-                const timerId = setInterval(() =>{
-                    addLog(task.name.toUppercase(),`Worker loop triggered every ${task.delay}s`, "task")
+        tasks.forEach((task) => {
+            if (task.status === "active") {
+                const timerId = setInterval(() => {
+                    addLog(task.name.toUppercase(), `Worker loop triggered every ${task.delay}s`, "task")
                 }, task.delay * 1000);
                 activeIntervals.push(timerId);
             }
         });
-        return ()=>{
+        return () => {
             activeIntervals.forEach((id) => clearInterval(id));
         };
 
 
     }, [tasks]);
 
-    const handleAddTask =(name,delay)    =>{
-        const newtask ={
+    const handleAddTask = (name, delay) => {
+        const newtask = {
             id: Date.now(),
-            name,delay,
-            status:"active"
+            name, delay,
+            status: "active"
         };
 
-        setTasks([...tasks,newtask]);
-        addLog("SYSTEM", `Task "${name}" added with a delay of ${delay}s`, "info");
+        setTasks([...tasks, newtask]);
+        addLog("SYSTEM", `Task : "${name}" `, "success");
 
-}
+    };
 };
