e4952457110807024046eb84ccfcf699bf76f59f
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-03 22:26:33 +0100

    importing components

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,18 @@ import React,{ useState, useEffect } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import TerminalLog from './components/TerminalLog';
+
+export default function App() { 
+    const [tasks, setTasks] = useState(() =>{
+        const savedTasks = localStorage.getItem('tasks');
+        return savedTasks ? JSON.parse(savedTasks) : [];
+    });
+
+    const[Logs, setLogs] = useState([]);
+
+    const addLog =(sender,message,type ='info') =>{
+
+        const timeStamp = new Date().toLocaleTimeString();
+    }
+   
+};
