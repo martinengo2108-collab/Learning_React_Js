9d443e28e5549ecc25899f94fd14ec65276cb2a6
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-04 10:31:17 +0100

    importing and exporting files

--- Task_scheduler/src/components/TaskForm.jsx
+++ Task_scheduler/src/components/TaskForm.jsx
@@ -3,6 +3,17 @@ import React ,{useState} from "react"
 export default function TaskForm({onAddTaskk}){
     const [taskName,setTaskname] = useState("");
 
-    const [intervalTime,setItervalTime] = useState("");
+    const [intervalTime,setIntervalTime] = useState("");
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+
+        if (!taskName.trim() || !intervalTime) return;
+
+        onAddTaskk(taskName,parseInt(intervalTime,10));
+
+        setTaskname;
+        setIntervalTime;
+    }
 
 }
