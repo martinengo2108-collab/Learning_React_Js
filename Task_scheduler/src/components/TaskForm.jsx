11487b8e02518bc880116aa66757db0e0c5b66fa
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-04 10:22:40 +0100

    importing and exporting files

---
+++ Task_scheduler/src/components/TaskForm.jsx
@@ -0,0 +1,8 @@
+import React ,{useState} from "react"
+
+export default function TaskForm({onAddTaskk}){
+    const [taskName,setTaskname] = useState("");
+
+    const [intervalTime,setItervalTime] = useState("");
+
+}
