4f57147b38eba198db74be1d2ecf39edddc72c1b
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-04 11:37:48 +0100

    importing and exporting files

---
+++ Task_scheduler/src/components/TerminalLog.jsx
@@ -0,0 +1,7 @@
+import React from"react";
+
+export default function TerminalLog({logs,onClear}){
+        return(
+            <div className="bg-slate-950 roundded-2xl border border-slate-800 p-5 flex flex-col shadow-2xl h-[530px]"></div>
+        )
+}
