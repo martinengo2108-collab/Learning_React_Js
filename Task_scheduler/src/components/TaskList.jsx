2df0bbb82d52e0552bb7ee0f484150850ec36314
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-04 11:11:42 +0100

    formating files

--- Task_scheduler/src/components/TaskList.jsx
+++ Task_scheduler/src/components/TaskList.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 
-export default function TaskList({tasks,onToggleStatus}){
-    return(
+export default function TaskList({ tasks, onToggleStatus }) {
+    return (
         <div>
             <h3
-            className="text-xs font-bold uppercase tracking-wider text-slate-400 mb-3 border-b border-slate-700 pb-2">
+                className="text-xs font-bold uppercase tracking-wider text-slate-400 mb-3 border-b border-slate-700 pb-2">
                 Registered tasks
 
             </h3>
+            {tasks.length === 0 ? (<p
+                className="text-sm text-slate-500 italic"
+            ></p>) : (
+                
+
+            )}
+
         </div>
     )
 }
