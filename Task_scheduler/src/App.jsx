e192f180a75058d168097ec8b873b793e7f1c266
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-07 14:57:11 +0100

    add task management functionality with state handling for tasks and logs

--- Task_scheduler/src/App.jsx
+++ Task_scheduler/src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import TerminalLog from "./components/TerminalLog";
 
 function App() {
+
+  const [tasks,setTasks] = useState([]);
+  const [logs,setLogs] =useState([])
+
+  const addTask =(taskName,intervalTime)=>{
+    const newTask ={
+      id:Date.now(),
+      name: taskName,
+      delay:intervalTime,
+      status:"active"
+    };
+
+    setTasks((currentTasks) =>[...currentTasks,newTask]);
+
+    setLogs((currentLogs) =>[
+      ...currenttLogs,{
+        
+      }
+    ])
+  }
   return (
     <div>
       <h1>Task Scheduler</h1>
