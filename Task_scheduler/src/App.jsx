4ded258e95c85b5fa7b7ab8278106bae186f2260
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-07 15:13:15 +0100

    refactor App component for improved readability and consistency in code formatting

--- Task_scheduler/src/App.jsx
+++ Task_scheduler/src/App.jsx
@@ -6,25 +6,44 @@ import TerminalLog from "./components/TerminalLog";
 
 function App() {
 
-  const [tasks,setTasks] = useState([]);
-  const [logs,setLogs] =useState([])
+  const [tasks, setTasks] = useState([]);
+  const [logs, setLogs] = useState([])
 
-  const addTask =(taskName,intervalTime)=>{
-    const newTask ={
-      id:Date.now(),
+  const addTask = (taskName, intervalTime) => {
+    const newTask = {
+      id: Date.now(),
       name: taskName,
-      delay:intervalTime,
-      status:"active"
+      delay: intervalTime,
+      status: "active"
     };
 
-    setTasks((currentTasks) =>[...currentTasks,newTask]);
+    setTasks((currentTasks) => [...currentTasks, newTask]);
 
-    setLogs((currentLogs) =>[
-      ...currenttLogs,{
-        
+    setLogs((currentLogs) => [
+      ...currentLogs,
+      {
+        timestamp: new Date().toLocaleTimeString(),
+        sender: "SYSTEM",
+        type: "sucess",
+        message: `Task"${taskName}" registered sucessfully.`
       }
-    ])
-  }
+    ]);
+  };
+
+  const toggleStatus = (id, name, status) => {
+    const newStatus = status === "active" ? "paused" : "active";
+
+    setTasks((currentTasks) =>
+      currentTasks.map((task) =>
+        task.id === id ? { ...task, status: newStatus }
+          : task)
+    );
+    setLogs((currentLogs) => [...currentLogs, {
+      timestamp: new Date().toLocaleTimeString(),
+      sender: "SYSTEM",
+      message: `Task "${name}" is now ${newStatus}.`
+    }]);
+  };
   return (
     <div>
       <h1>Task Scheduler</h1>
