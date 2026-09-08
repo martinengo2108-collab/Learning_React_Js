e05061f7f984dbd0661e6f90ba40ce98fefd2a91
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-08 17:30:32 +0100

    Refactor TerminalLog component for improved log message styling

--- Task_scheduler/src/components/TerminalLog.jsx
+++ Task_scheduler/src/components/TerminalLog.jsx
@@ -42,14 +42,14 @@ export default function TerminalLog({logs,onClear}){
                         return(
                             <div
                             key={index}
-                            className="leading-relaxed border-b border-slate-900/50 pb-1.5 last:border-0">
+                            className="leading-relaxed border-b border-slate-100 pb-1.5 last:border-0">
                                 <span
                                 className="text-slate-500 mr-2">[{log.timesstamp}]</span>
                                 <span className={`px-1.5 py-0.5 rounded text-[10px] uppercase font-boldd tracking-wide mr-2 ${tagstyle}`}>
                                     {log.sender}
                                 </span>
                                 <span
-                                className={log.type ==="task" ? "text-purple-200" : "text-slate-300"}>{log.message}
+                                className="text-slate-700">{log.message}
 
                                 </span>
 
