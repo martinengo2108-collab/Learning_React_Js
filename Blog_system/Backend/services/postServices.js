9d4c08e263847b97dbad1b94de9b69523ee5c1c8
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-09 16:50:45 +0100

    fix: improve code formatting and consistency in postServices.js

--- Blog_system/Backend/services/postServices.js
+++ Blog_system/Backend/services/postServices.js
@@ -1,26 +1,41 @@
-const API_URL ="https://jsonplaceholder.typicode.com/posts"
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
 
-export async function getPosts(){
-    const response=await fetch(API_URL);
+export async function getPosts() {
+    const response = await fetch(API_URL);
 
-    if(!response.ok){
+    if (!response.ok) {
 
         throw new Error("Failed to fetch posts");
     }
 
     return response.json();
 }
-export async function createPost(postData){
-    const response = await fetch(API_URL,{
-        method:"POST",
+export async function createPost(postData) {
+    const response = await fetch(API_URL, {
+        method: "POST",
 
-        headers:{
-            "Content-type":"application/json"
+        headers: {
+            "Content-type": "application/json"
         },
-        body:JSON.stringify(postData)
+        body: JSON.stringify(postData)
     });
-    if (!response.ok){
+    if (!response.ok) {
         throw new Error("Failed to create post");
     }
     return response.json();
+}
+
+export async function updatePost(id, postData) {
+
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postData)
+    });
+    if (!response.ok) {
+        throw new Error("Failed to delete post");
+    }
+    return true;
 }
