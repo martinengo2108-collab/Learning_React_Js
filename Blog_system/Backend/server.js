019265b201a2fb3d16c8b37431f6b9afe6c8afbd
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-09 16:14:09 +0100

    fix: add missing semicolon and newline in server.js

--- Blog_system/Backend/server.js
+++ Blog_system/Backend/server.js
@@ -15,4 +15,9 @@ app.get("/", (req, res) => {
     });
 });
 
-app.use("/api/posts", postRoutes)
+app.use("/api/posts", postRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+
+});
