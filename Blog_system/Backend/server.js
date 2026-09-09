e4fb073dc88ea78613cb90f2687cc49d759b8f55
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-09 16:07:26 +0100

    initialize server with express and cors middleware

--- Blog_system/Backend/server.js
+++ Blog_system/Backend/server.js
@@ -2,3 +2,9 @@ import express from "express";
 import cors from "cors";
 
 import postRoutes from "./routes/postRoutes";
+
+const app =express();
+const PORT =5000;
+
+app.use(cors());
+app.use(express.json())
