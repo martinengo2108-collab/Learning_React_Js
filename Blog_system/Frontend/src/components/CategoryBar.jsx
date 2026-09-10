d6157fb8e1b5dac05205decf4f58921d3d45805a
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 14:21:37 +0100

    add CategoryBar component for category selection

---
+++ Blog_system/Frontend/src/components/CategoryBar.jsx
@@ -0,0 +1,30 @@
+function CategoryBar({ selectedCategory, onCategoryChange }) {
+    const categories = [
+        "All",
+        "Academics",
+        "Campus Life",
+        "Technology",
+        "Career",
+        "Personal Growth",
+        "internship",
+    ];
+    return (
+        <div className="category-wrapper">
+            <div className="category-bar">
+                {categories.map((category)=>(
+                <button
+                    key={category}
+                    className={
+                        selectedCategory === category?
+                        "category-button active":"category-button"
+                    }
+                    onClick={()=>onCategoryChange(category)}>
+                        {category}
+                    </button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default CategoryBar;
