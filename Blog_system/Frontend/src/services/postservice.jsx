fcda80700c6f66b01526fd3bf074ddff6d14d2fa
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 13:08:21 +0100

    formating documents

--- Blog_system/Frontend/src/services/postservice.jsx
+++ Blog_system/Frontend/src/services/postservice.jsx
@@ -1,6 +1,12 @@
-const API_URL="https://jsonplaceholder.typicode.com/posts"
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
 
-export async function getPosts(){
-    
+export async function getPosts() {
+    const response = await fetch(API_URL);
+
+    if(!response.ok){
+        throw new Error("Failed to fetch posts");
+    }
+
+    return response.json();
 
 }
