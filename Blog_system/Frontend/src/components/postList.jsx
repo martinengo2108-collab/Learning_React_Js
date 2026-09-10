6597e549817a715455d9d36b0e7758fff0e2aac6
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 15:23:02 +0100

    fix PostList component to correctly render EmptyState when no posts are available

--- Blog_system/Frontend/src/components/postList.jsx
+++ Blog_system/Frontend/src/components/postList.jsx
@@ -4,7 +4,17 @@ import EmptyState from "./EmptyState";
 
 function PostList({posts,onDelete}){
     if(postCard.length===0){
-        
+    
         return <EmptyState />
     }
+    return(
+        <div className="post-grid">
+            {postCard.map((post)=>(
+                <postCard
+                key={post.id}
+                post={post}
+                onDelete={onDelete}/>
+            ))}
+        </div>
+    )
 }
