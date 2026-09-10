b8add6981fd3d9c9a174b0e1d635d5ca19ddc69c
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 13:55:04 +0100

    add Hero component with student voices message

---
+++ Blog_system/Frontend/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import { link } from "react-router-dom";
+
+function Hero(){
+    return(
+        <section
+        className="hero">
+            <div className="hero-content">
+                <span
+                className="hero-label">
+                  ✨ An environment for student voices
+
+                </span>
+            </div>
+        </section>
+    )
+}
