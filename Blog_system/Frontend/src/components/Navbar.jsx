c6b320bcf94ff4cfd02e4ab93373fc4d323cb761
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 13:44:58 +0100

    add Navbar component with navigation links and logo

---
+++ Blog_system/Frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { link } from "react-router-dom";
+
+function Navbar(){
+    return(
+        <nav
+        className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="logo">
+                    <span
+                    className="logo-icon">✦</span>
+                    StudentSphere
+                </Link>
+                <div className="nav-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/category">Categories</Link>
+                    <Link to="/">About</Link>
+                </div>
+                <Link to="/create"
+                className="share-button">
+                    ✍Share your campus journey.
+                </Link>
+            </div>
+
+        </nav>
+    )
+}
