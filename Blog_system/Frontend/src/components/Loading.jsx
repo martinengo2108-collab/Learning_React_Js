2dd14fe780cbc692dc3f79a7cb0d65fbd430059f
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 16:16:26 +0100

    exporting

--- Blog_system/Frontend/src/components/Loading.jsx
+++ Blog_system/Frontend/src/components/Loading.jsx
@@ -5,6 +5,10 @@ function EmptyState(){
                 🔎
 
             </div>
+            <h3>No story found</h3>
+            <p>We couldn't find any story matching your research</p>
+
         </div>
-    )
+    );
 }
+export default EmptyState;
