ae33c79d9e7d765a924490e8f99e82f24c353980
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 14:03:42 +0100

    refactor Hero component for improved structure and readability

--- Blog_system/Frontend/src/components/Hero.jsx
+++ Blog_system/Frontend/src/components/Hero.jsx
@@ -1,15 +1,42 @@
 import { link } from "react-router-dom";
 
-function Hero(){
-    return(
+function Hero() {
+    return (
         <section
-        className="hero">
+            className="hero">
             <div className="hero-content">
                 <span
-                className="hero-label">
-                  ✨ An environment for student voices
+                    className="hero-label">
+                    ✨ An environment for student voices
 
                 </span>
+                <h1>Learn from <br />experience <br />
+                    ,change your <span>
+                        perspective</span></h1>
+
+                <p>Discover real experiences from students,share what you've learned,
+                    and inspire someone on their journey.
+                </p>
+
+                <div className="hero-actions">
+                    <Link to="/create" className="hero-primary">
+                        Share your story </Link>
+                    <a href="#discover"
+                        className="hero-secondary">
+                        Explore stories
+                    </a>
+                </div>
+            </div>
+            <div className="hero-decoration">
+                <div className="floating-card card-one">
+                    💡
+                    <span>Share Ideas</span>
+
+                </div>
+
+                <div className="floating-card card-two">
+                    
+                </div>
             </div>
         </section>
     )
