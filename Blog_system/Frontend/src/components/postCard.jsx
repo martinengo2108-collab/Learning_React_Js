4189027e12f77089c966b28beae0e0ad0f54768e
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 15:07:19 +0100

    format postCard component for improved structure and readability

--- Blog_system/Frontend/src/components/postCard.jsx
+++ Blog_system/Frontend/src/components/postCard.jsx
@@ -3,12 +3,26 @@ import { Link } from "react-router-dom";
 
 function postCard({ post, onDelete }) {
     const category = post.category || "Studennt Life";
-    return(
+    return (
         <article className="post-card">
 
             <div className="post-card-top">
                 <span className="post-category">
                     {category}
+                </span>
+                <span
+                    className="post-number">
+                    #{post.id}
+                </span>
+            </div>
+            <h3>{post.title}</h3>
+            <p>
+                {post.body.length > 130
+                    ? `${post.body.substring(0, 130)}...` : post.body}
+            </p>
+            <div className="post-card-footer">
+                <span className="author">
+                    By Student
                 </span>
             </div>
 
