ee497e9f99ba6b080bc5e13222fe4dca77290901
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 13:12:05 +0100

    getting one post

--- Blog_system/Frontend/src/services/postservice.jsx
+++ Blog_system/Frontend/src/services/postservice.jsx
@@ -10,3 +10,14 @@ export async function getPosts() {
     return response.json();
 
 }
+export async function getPost(id){
+    const response=await fetch(`${API_URL}/${id}`);
+    
+    if(!response.ok){
+        throw new Error("Failed to fetch posts");
+
+    }
+    return response.json();
+
+}
+
