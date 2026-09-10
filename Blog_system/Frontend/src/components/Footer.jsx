0158ea8a57feecd65426f5051d1f1b95d4a5553f
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 17:28:07 +0100

    update Footer component with additional links and description

--- Blog_system/Frontend/src/components/Footer.jsx
+++ Blog_system/Frontend/src/components/Footer.jsx
@@ -11,8 +11,26 @@ function Footer(){
                     className="logo">
                         <span
                         className="logo-icon">✦</span>
+                        StudentSphere
                     </Link>
+                    
+                    <p>
+                        A space where students share experiences, ideas and stories that matter.
+                    </p>
                 </div>
+                <div className="footer-links">
+                    <h4>Explore</h4>
+                    <Link to="/"> Home</Link>
+                    <Link to="/category"> Categories</Link>
+                    <Link to="/about"> About</Link>
+                </div>
+                <div className="footer-links">
+                    <h4>Community</h4>
+
+                    <Link to="/create">Share your story</Link>
+                    <Link to="/discover">Discover stories</Link>
+                </div>
+
             </div>
         </footer>
     )
