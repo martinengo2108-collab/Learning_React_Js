a1b7ee15d927f7062de7211ab426fb7108cf4efc
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 15:19:11 +0100

    add PostList component logic to render EmptyState when no posts are available

--- Blog_system/Frontend/src/components/postList.jsx
+++ Blog_system/Frontend/src/components/postList.jsx
@@ -1,3 +1,10 @@
 import postCard from "./postCard";
 
 import EmptyState from "./EmptyState";
+
+function PostList({posts,onDelete}){
+    if(postCard.length===0){
+        
+        return <EmptyState />
+    }
+}
