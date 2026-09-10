4d18134db6e0da530295df8147e25bac243d1a7a
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 19:04:34 +0100

    fix: correct import statement and variable naming in Category component

--- Blog_system/Frontend/src/pages/Category.jsx
+++ Blog_system/Frontend/src/pages/Category.jsx
@@ -1,1 +1,29 @@
-import { useefffect, useState } from "react";
+import { useEffect, useefffect, useState } from "react";
+
+import CategoryBar from "../components/CategoryBar";
+import PostList from "../components/postList";
+import Loading from"../components/Loading";
+import{getPosts}from"../services/postservice";
+
+function Category(){
+    const[posts,setPosts]=useState([]);
+    const[category,setCategory]=useState("All");
+
+    const [loading,setLoading]=useState(true);
+    useEffect(()=>{
+        async function loadPosts(){
+            try{
+                const dat=await getPosts();
+
+                setPosts(data);
+            }finally{
+                setLoading(false)
+            }
+        }
+        loadPosts();
+    },[]);
+    const filteredPosts=
+    category==="All"?
+    posts:
+    posts.filter()
+}
