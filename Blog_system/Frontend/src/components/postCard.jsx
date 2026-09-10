1753b8727ca9dcfe3e06de513955952c149e99e9
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 15:01:23 +0100

    add missing semicolon and format postCard component

--- Blog_system/Frontend/src/components/postCard.jsx
+++ Blog_system/Frontend/src/components/postCard.jsx
@@ -2,5 +2,16 @@ import { Link } from "react-router-dom";
 
 
 function postCard({ post, onDelete }) {
-    const category = post.category || "Studennt Life"
+    const category = post.category || "Studennt Life";
+    return(
+        <article className="post-card">
+
+            <div className="post-card-top">
+                <span className="post-category">
+                    {category}
+                </span>
+            </div>
+
+        </article>
+    )
 }
