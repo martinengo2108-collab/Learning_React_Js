221b537d7d47326f52a9f6293d3cde1460083d5d
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 13:15:49 +0100

    formating documents

--- Blog_system/Frontend/src/services/postservice.jsx
+++ Blog_system/Frontend/src/services/postservice.jsx
@@ -3,17 +3,17 @@ const API_URL = "https://jsonplaceholder.typicode.com/posts"
 export async function getPosts() {
     const response = await fetch(API_URL);
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error("Failed to fetch posts");
     }
 
     return response.json();
 
 }
-export async function getPost(id){
-    const response=await fetch(`${API_URL}/${id}`);
-    
-    if(!response.ok){
+export async function getPost(id) {
+    const response = await fetch(`${API_URL}/${id}`);
+
+    if (!response.ok) {
         throw new Error("Failed to fetch posts");
 
     }
@@ -21,3 +21,13 @@ export async function getPost(id){
 
 }
 
+export async function createPost(post) {
+    const response = await fetch(API_URL, {
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+    })
+}
