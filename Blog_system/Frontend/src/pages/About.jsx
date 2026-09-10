ac883b886ea32e2cddab7c7af4b9ee9caae49e38
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-10 19:25:31 +0100

    add About component with initial layout and structure

---
+++ Blog_system/Frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+function About(){
+    return(
+        <main
+        className="about-page">
+            <section className="about-hero">
+                <span
+                className="section-label">
+                    ABOUT StudentSphere
+                </span>
+            </section>
+        </main>
+    )
+}
