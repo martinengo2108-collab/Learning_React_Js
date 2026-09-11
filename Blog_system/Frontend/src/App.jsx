e30f99b3be0721231c90a564bdcc8cd187dd88c8
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-11 10:58:06 +0100

    fix formatting issues in App.jsx for consistent import statements and improved readability

--- Blog_system/Frontend/src/App.jsx
+++ Blog_system/Frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import NavBar from"./components/Navbar";
-import Footer from"./components/Footer";
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import About from "./pages/About";
 import PostDetails from "./pages/PostDetails";
 import CreatePost from "./pages/createPost";
 import EditPost from "./pages/EditPost";
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
