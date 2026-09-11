8255c75bdbb0e720495e8621dc4f7b43e6436edd
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-11 09:50:27 +0100

    correct import statement and improve loadPost function in EditPost component

--- Blog_system/Frontend/src/pages/EditPost.jsx
+++ Blog_system/Frontend/src/pages/EditPost.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { useNavigate, useParams } from "react-router-dom";
+import { data, useNavigate, useParams } from "react-router-dom";
 
 import PostForm from "../components/postForm";
 import { createPost } from "../services/postservice";
@@ -18,5 +18,23 @@ function EditPost() {
     const[post,setPost]=useState(null);
 
     const [loading,setLoading]=useState(true);
-    
+
+    useEffect(()=>{
+        async function loadPost() {
+
+            try{
+                const data=await getPost(id);
+
+                setPost(data);
+            }catch{
+                alert("Unable to load this story")
+            }finally{
+                setLoading(false);
+            }
+            
+        }
+
+        loadPost()
+    },[id]);
+
 }
