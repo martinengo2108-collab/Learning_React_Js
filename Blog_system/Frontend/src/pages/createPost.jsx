a476457a7882e1f9a724d444315ae1c69a245b9b
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-11 09:38:25 +0100

    implement CreatePost component with loading state and submission logic

--- Blog_system/Frontend/src/pages/createPost.jsx
+++ Blog_system/Frontend/src/pages/createPost.jsx
@@ -1,5 +1,52 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import PostForm from"../components/postForm";
+import PostForm from "../components/postForm";
 import { createPost } from "../services/postservice";
+
+function CreatePost() {
+
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+
+    const handleCreate=async (post)=>{
+        try{
+            setLoading(true);
+
+            const newPost=await createPost(post);
+
+            navigate(`/posts/${newPost.id}`);
+        }catch{
+            alert("Unable to publish your story.");
+        }finally{
+            setLoading(false)
+        }
+    };
+
+    return(
+        <main
+        className="form-page">
+            <div className="form-header">
+                <span className="section-label">
+                    SHARE YOUR EXPERIENCE
+                    
+                </span>
+                <h1>
+                    Tell your story
+                </h1>
+
+                <p>
+                    Your experiences could inpire another student.
+                </p>
+            </div>
+            <PostForm
+            onSubmit={handleCreate}
+            submitText={
+                loading?"Publishing...":"Publish story"
+            }
+
+            />
+        </main>
+    );
+}
