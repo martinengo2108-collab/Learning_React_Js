012fd1ba316515cddc618a97351f773e54ff0488
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-11 09:06:22 +0100

    seetting constants

--- Blog_system/Frontend/src/pages/PostDetails.jsx
+++ Blog_system/Frontend/src/pages/PostDetails.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-import{getPost}from"../services/postservice";
-import Loading from"../components/Loading";
+import { getPost } from "../services/postservice";
+import Loading from "../components/Loading";
+
+function PostDetails() {
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+}
