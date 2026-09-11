fcd3ab3abf3ce7f967959eb36c7d449cc28df1bf
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-11 09:46:01 +0100

    importing

--- Blog_system/Frontend/src/pages/EditPost.jsx
+++ Blog_system/Frontend/src/pages/EditPost.jsx
@@ -1,6 +1,22 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-import { useNavigate,useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import PostForm from "../components/postForm";
 import { createPost } from "../services/postservice";
+import {
+    getPost,
+    updatePost,
+} from "../services/postservice";
+
+
+function EditPost() {
+    const { id } = useParams();
+
+    const navigate=useNavigate();
+
+    const[post,setPost]=useState(null);
+
+    const [loading,setLoading]=useState(true);
+    
+}
