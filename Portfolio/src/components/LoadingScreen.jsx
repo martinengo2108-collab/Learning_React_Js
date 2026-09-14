09a9fc0bff0c979fdf19835b67c7818a740e286a
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-14 17:48:57 +0100

    correct useEffect implementation and update text setting logic in LoadingScreen component

--- Portfolio/src/components/LoadingScreen.jsx
+++ Portfolio/src/components/LoadingScreen.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const LoadingScreen = () => {
 
     const [text, setText] = useState("");
 
-    const fullText="<Welcome />"
+    const fullText = "<Welcome />";
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+
+            setText(fullText, substring(0, index));
+            index++;
+
+            if (index > fullText.length){
+                
+            }
+        
+        })
+    })
 
     return <div className="fixed inset-0  z-50 bg-black text-grey-100 flex flex-col items-center justify-center">
         <div className="mb-4 text-4xl font-mono font-bold">
