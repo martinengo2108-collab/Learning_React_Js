56862f778badde303d5a54f56ad16e1d1718e400
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-14 17:35:10 +0100

    add LoadingScreen component to App and enhance its structure

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
+import {LoadingScreen} from './components/LoadingScreen';
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    <LoadingScreen/>
      
     </>
   )
