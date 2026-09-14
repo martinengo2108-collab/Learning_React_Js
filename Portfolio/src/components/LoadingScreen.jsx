d0b99d3286079e7206e48fab6d9247106b0fce23
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-14 17:21:12 +0100

    add greeting message to LoadingScreen component

--- Portfolio/src/components/LoadingScreen.jsx
+++ Portfolio/src/components/LoadingScreen.jsx
@@ -1,7 +1,10 @@
 export const LoadingScreen=()=>{
 
     return <div className="fixed inset-0  z-50 bg-black text-grey-100 flex flex-col items-center justify-center">
-        <div className="mb-4 text-4xl font-mono font-bold"></div>
+        <div className="mb-4 text-4xl font-mono font-bold">
+            Hello World 
+            <span className="animate-blink ml-1"> | </span>
+        </div>
 
     </div>
 }
