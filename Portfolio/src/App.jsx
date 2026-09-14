5b51b79ff377f14753d9213d267b0b52dfece2a7
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-14 17:53:37 +0100

    Update App component to use boolean state for count and conditionally render LoadingScreen

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import {LoadingScreen} from './components/LoadingScreen';
 import "./index.css";
+
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(false)
 
   return (
     <>
-    <LoadingScreen/>
+    {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}
      
     </>
   )
