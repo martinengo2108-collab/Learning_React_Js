6ab66a36b94c532dc5d2d1b36679cb0aa7148715
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-15 11:24:12 +0100

    Enhance Home component with improved structure and styling for better presentation

--- Portfolio/src/components/sections/Home.jsx
+++ Portfolio/src/components/sections/Home.jsx
@@ -1,8 +1,11 @@
 export const Home =()=>{
     return<section id="home"
     className="min-h-screen flex items-center justify-center relative">
-        <div className="text-center">
-            
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 gradient-text leading-rigth">
+                Hi, I am Martine
+            </h1>
+
         </div>
     </section>
 }
