42396a33f80f8b8f1d3f745cdf4ed17200a8f7de
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-15 12:51:12 +0100

    Add About component and update App to include it for improved structure

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { Home } from './components/sections/Home';
 import { Navbar } from "./components/Navbar"
 import { MobileMenu } from './components/MobileMoney';
+import { About } from './components/sections/About';
 
 import viteLogo from './assets/vite.svg'
 import './App.css'
@@ -22,8 +23,9 @@ function App() {
           : "opacity-0"}bg-black text-gray-100`} >
 
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        
+
         <Home />
 
         <About />
