084b2ed03c991019ad64264c7efd399b3ace3c8b
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-16 09:42:31 +0100

    styling issue in About component and add technology tags to Projects section

--- Portfolio/src/components/sections/Project.jsx
+++ Portfolio/src/components/sections/Project.jsx
@@ -16,6 +16,17 @@ export const Projects = () => {
                     <p>
                         scelhifhei
                     </p>
+                    <div>
+                        {["React","Node.js"].map((tech,key)=>{
+
+                            <span
+                                key={key}
+                                        className="bg-blue-500/10 text-blue py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                        {tech}
+                            </span>
+                        })}
+                    </div>
                 </div>
             </div>
 
