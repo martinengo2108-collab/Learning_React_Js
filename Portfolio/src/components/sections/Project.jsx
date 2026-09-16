aec04e05da8d2d784628ffe70f1210075f679163
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-16 09:53:39 +0100

    formatting

--- Portfolio/src/components/sections/Project.jsx
+++ Portfolio/src/components/sections/Project.jsx
@@ -1,39 +1,40 @@
 export const Projects = () => {
 
     return (
-    <section id="projects" className="min-h-screen flex items-center justify-center py-20">
-        <div className="max-w-5xl mx-auto px-4">
-            <h2 className="text-3xl font-bold mb-8 bg-gradient from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-               {" "} Featured Projects
+        <section id="projects" className="min-h-screen flex items-center justify-center py-20">
+            <div className="max-w-5xl mx-auto px-4">
+                <h2 className="text-3xl font-bold mb-8 bg-gradient from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+                    {" "} Featured Projects
 
-               {" "}
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div>
-                    <h3>
-                        Cloud Platform
-                    </h3>
-                    <p>
-                        scelhifhei
-                    </p>
-                    <div>
-                        {["React","Node.js"].map((tech,key)=>{
+                    {" "}
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1
+                     hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                        <h3 >
+                            Cloud Platform
+                        </h3>
+                        <p>
+                            scelhifhei
+                        </p>
+                        <div>
+                            {["React", "Node.js"].map((tech, key) => {
 
-                            <span
-                                key={key}
-                                        className="bg-blue-500/10 text-blue py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                                        {tech}
-                            </span>
-                        })}
+                                <span
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition-all">
+                                    {tech}
+                                </span>
+                            })}
+                        </div>
                     </div>
                 </div>
+
             </div>
 
-        </div>
 
 
-
-    </section>)
+        </section>)
 
 }
