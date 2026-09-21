cb131d4238c191ed2383a7c64d40173efef55435
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-21 14:23:06 +0100

    correct route path for getting a post by ID in postRoutes

--- Blog_system/Backend/routes/postRoutes.js
+++ Blog_system/Backend/routes/postRoutes.js
@@ -12,6 +12,9 @@ const router = express.Router();
 
 router.get("/",getPosts);
 
-router.get(":id",getPostById);
+router.get("/:id",getPostById);
 
 router.post("/",createPost);
+
+router.put("/:id",updatePost);
+router.delete("/:id",deletePost);
