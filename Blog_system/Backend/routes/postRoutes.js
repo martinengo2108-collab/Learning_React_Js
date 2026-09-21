e04499c77fd1b7d9f4416f849eb1e25ab11a561d
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-21 14:23:29 +0100

    formatting

--- Blog_system/Backend/routes/postRoutes.js
+++ Blog_system/Backend/routes/postRoutes.js
@@ -10,11 +10,11 @@ import {
 
 const router = express.Router();
 
-router.get("/",getPosts);
+router.get("/", getPosts);
 
-router.get("/:id",getPostById);
+router.get("/:id", getPostById);
 
-router.post("/",createPost);
+router.post("/", createPost);
 
-router.put("/:id",updatePost);
-router.delete("/:id",deletePost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
