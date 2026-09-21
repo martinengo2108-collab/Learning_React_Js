f33e53c0b844ca65e07b5c79ece2995d9d34621d
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-21 15:39:27 +0100

    route handlers in postRoutes.js for consistency and clarity

--- Blog_system/Backend/routes/postRoutes.js
+++ Blog_system/Backend/routes/postRoutes.js
@@ -2,8 +2,8 @@ import express from "express";
 
 import {
     getPosts,
-    getPostById,
-    createPost,
+    getPost,
+    addPost,
     updatePost,
     deletePost
 } from "../controllers/postController.js";
@@ -12,11 +12,12 @@ const router = express.Router();
 
 router.get("/", getPosts);
 
-router.get("/:id", getPostById);
+router.get("/:id", getPost);
 
-router.post("/", createPost);
+router.post("/", addPost);
 
 router.put("/:id", updatePost);
+
 router.delete("/:id", deletePost);
 
 export default router;
