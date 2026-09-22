f5ea0cc73d2b4b3219fcba57fb8bce4a16b9fc46
martinengo2108-collab <martinengo2108@gmail.com>
2026-09-22 17:27:10 +0100

    feat: add useState for managing form data in Contact component

--- Portfolio/src/components/sections/Contact.jsx
+++ Portfolio/src/components/sections/Contact.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import RevealOnScroll from "../RevealOnScroll";
 
 
 import emailjs from "emailjs-com";
 
 export const Contact = () => {
+    const [formData,setFormData]=useState({
+        name:"",
+        email:"",
+        message:"",
+    });
 
     const SERVICE_ID = "service_diqweqi";
 
